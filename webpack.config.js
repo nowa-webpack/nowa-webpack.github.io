89cb6e445eafa1f987197a506715762402057d21
gbk <ck0123456@gmail.com>
2016-12-01 17:23:10 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: gbk
 * @Date:   2016-06-10 18:37:02
 * @Last Modified by:   gbk
-* @Last Modified time: 2016-06-10 18:53:12
+* @Last Modified time: 2016-12-01 17:22:33
 */
 
 'use strict';
@@ -11,10 +11,6 @@ const path = require('path');
 
 module.exports = (config) => {
   config.module.loaders.push({
-    test: /\.ttf$/,
-    loader: 'url',
-    include: path.join(process.cwd(), 'src')
-  }, {
     test: /\.handlebars$/,
     loader: 'handlebars',
     include: path.join(process.cwd(), 'src')
